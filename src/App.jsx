f718f7f1061d7833ddd0adc3708923f9745d2a01
Mehedi Hasan <hasanmdrifat77@gmail.com>
2024-07-13 06:32:54 +0600

    add

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,17 @@ export default function App() {
       <div className="h-screen  bg-gradient-to-r from-cyan-500 to-blue-500 flex justify-center items-center">
         <div className="h-4/5	w-1/3 bg-white rounded p-10">
           <div className="h-full flex flex-col justify-around">
-            <h1 className="text-5xl text-center font-bold text-black ">
+            <h1 className="text-5xl text-center font-bold text-black bg-slate-400 p-10">
               Log In
             </h1>
             <form className="flex flex-col gap-10 justify-center items-start">
               <input
-                className="bg-slate-300 outline-none ps-5 py-3 w-11/12"
+                className="bg-slate-300 outline-none ps-5 py-3 w-full"
                 type="text"
                 placeholder="Type your name"
               ></input>
               <input
-                className="bg-slate-300 outline-none ps-5 py-3 w-11/12"
+                className="bg-slate-300 outline-none ps-5 py-3 w-full"
                 type="password"
                 placeholder="Type your password"
               ></input>
