aad7686633bc99823299c78372a91fe6abc23151
Mehedi Hasan <hasanmdrifat77@gmail.com>
2024-07-13 06:03:22 +0600

    add

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+export default function App() {
+  return (
+    <>
+      <div className="h-screen  bg-gradient-to-r from-cyan-500 to-blue-500 flex justify-center items-center">
+        <div className="h-4/5	w-1/3 bg-white rounded p-10">
+          <div className="h-full flex flex-col justify-around">
+            <h1 className="text-5xl text-center font-bold text-black ">
+              Log In
+            </h1>
+            <form className="flex flex-col gap-10 justify-center items-start">
+              <input
+                className="bg-slate-300 outline-none ps-5 py-3 w-11/12"
+                type="text"
+                placeholder="Type your name"
+              ></input>
+              <input
+                className="bg-slate-300 outline-none ps-5 py-3 w-11/12"
+                type="password"
+                placeholder="Type your password"
+              ></input>
+              <input
+                className="bg-[#0088ff] px-3 py-2 text text-2xl uppercase font-semibold rounded-sm cursor-pointer opacity-100 hover:opacity-85"
+                type="submit"
+              ></input>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{css,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
